30de67569b8532719686e5ad0ecb06a733315343
mlakota <miroslaw.lakota@gmail.com>
2010-08-10 16:24:06 +0200

    Save img and thumbnails

--- imgProcessor.py
+++ imgProcessor.py
@@ -9,26 +9,42 @@ class IMG(object):
 	srcPath = ""
 	destPath = ""
 	imgTree = []
+	thumbPattern = ""
 
 
 	def __init__(self):
 		pass
 
 	def process(self):
-		pass
+		for i in self.imgTree:
+			self.__processElement(self.srcPath,
+				self.destPath, i)
 	
-	def __processElement(self, source, dest):
-		if isinstance(source,'dict'):
-			for i in source.values()[0]:
-				print i
+	def __processElement(self, source, dest, elem):
+		if isinstance(elem,dict):
+			for i in elem.values()[0]:
+				self.__processElement(source+os.sep+
+					elem.keys()[0], dest+os.sep+
+					elem.keys()[0], i)
+		else:
+			if not osp.exists(dest):
+				os.mkdir(dest)
+			im = Image.open(source+os.sep+elem)
+			im.thumbnail((self.imgW,self.imgH))
+			im.save(dest+os.sep+elem)
+			im.thumbnail((self.thumbW,self.thumbH))
+			im.save(dest+os.sep+self.thumbPrefix+elem)
 
 def main():
 	img = IMG()
 	img.srcPath = 'E:\Fotki'
-	img.destPath = 'E:\Galeria'
-	img.imgW = img.imgH = 600
+	img.destPath = 'E:\Galeria\zdjecia'
+	img.imgW = 600
+	img.imgH = 600
 	img.thumbW = img.thumbH = 100
-	img.imgTree = ['Chrysantemum.jpg', 'Desert.jpg']
+	img.thumbPrefix="th_"
+	img.imgTree = ['Chrysanthemum.jpg', 'Desert.jpg', {'folder2' :
+	['Koala.jpg', 'Lighthouse.jpg']}]
 	img.process()
 
 if __name__ == '__main__':
