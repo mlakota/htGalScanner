a6d0359d253e8731c951f5e0dc714003419afa7b
mlakota <miroslaw.lakota@gmail.com>
2010-08-10 10:33:45 +0200

    Change method name to debug()

--- htGalScanner.py
+++ htGalScanner.py
@@ -6,11 +6,12 @@ class HTScan(object):
 	def __init__(self,args):
 		self.args = args
 
-	def print(self):
+	def debug(self):
 		print self.args
 
 def main(*args):
-	print args
+	HT = HTScan(args)
+	HT.debug()
 
 if __name__ == '__main__':
 	main('arg1','arg2')
